7b6971aad270704c6579ba92cc95c577a69c8607
abhilashpraharaj122@gmail.com <abhilashpraharaj122@gmail.com>
2025-06-21 19:59:46 +0530

    added a hashmap to store the hash

--- src/hash.rs
+++ src/hash.rs
@@ -1,8 +1,10 @@
 use sha2::{Sha256, Digest};
 use std::fs::File;
 use std::io::{BufReader,Read};
+use walkdir::WalkDir;
+use std::collections::HashMap;
 
-pub fn same(path1: &String, path2: &String) -> bool{
+/*pub fn same(path1: &String, path2: &String) -> bool{
 
   let mut hash1: String= hashed(path1);
   let mut hash2: String= hashed(path2);
@@ -12,9 +14,43 @@ pub fn same(path1: &String, path2: &String) -> bool{
   }else{
     false
   }
+}*/
+
+pub fn same(path: &String) -> bool{
+
+  let mut hashed: HashMap<String,i32> = HashMap::new();
+  let mut flag: bool = true;
+
+  for entry in WalkDir::new(path){
+
+    let entry = entry.unwrap();
+    let path = entry.path();
+
+    if path.is_file(){
+      let mut hash: String = hashing(&path.to_string_lossy().to_string());
+      if let Some(val) = hashed.get_mut(&hash){
+        flag = false;
+        *val += 1;
+      }else{
+        hashed.insert(hash.clone(),1);
+      }
+    }else{
+      continue;
+    }
+  }
+
+  for (key,value) in &hashed{
+    if *value > 1{
+      println!("{}",key);
+    }
+  }
+  flag
 }
 
-fn hashed(path: &String) -> String{
+
+
+
+fn hashing(path: &String) -> String{
   let file = File::open(path).unwrap();
 
   let mut reader = BufReader::new(file);
--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,9 @@ fn main() {
 
             match input {
                 None => {
-                    let f1 = r"E:\test\f1.txt".to_string();
-                    let f2 = r"E:\test\f2.txt".to_string();
-                    if hash::same(&f1,&f2){
+                    let f1 = r"E:\test".to_string();
+                    //let f2 = r"E:\test\f2.txt".to_string();
+                    if hash::same(&f1){
                         println!("jai shri ram");
                     }else{
                         println!("alla hu akhbar");
