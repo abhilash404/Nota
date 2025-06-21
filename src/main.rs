00be473a3f30a1f03db859c459c3b6e070faf8bf
abhilashpraharaj122@gmail.com <abhilashpraharaj122@gmail.com>
2025-06-21 20:56:32 +0530

    added the feature to check the directory itself

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod args;
 mod hash;
 
+use std::env;
 use args::{Cli,Args};
 use clap::Parser;
 
@@ -12,15 +13,22 @@ fn main() {
 
             match input {
                 None => {
-                    let f1 = r"E:\test".to_string();
+                    let current_dir = env::current_dir().unwrap();
+                    let f1 = current_dir.to_string_lossy().to_string();
                     //let f2 = r"E:\test\f2.txt".to_string();
                     if hash::same(&f1){
-                        println!("jai shri ram");
+                        println!("No duplicates in the file");                       
                     }else{
-                        println!("alla hu akhbar");
+                        println!("there are duplicate files in the directory given.\n the hash of those are:\n");
                     }
                 },
-                Some(name) => println!("le {}",name),
+                Some(name) => {
+                    if hash::same(&name){
+                        println!("No duplicates in the file");
+                    }else{
+                        println!("there are duplicate files in the directory given.\n the hash of those are given above:\n");
+                    }
+                },
             }
             
         },
